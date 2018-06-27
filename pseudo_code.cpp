2b9401b586702696413da7480f521055e5c29865
kjy276 <kjy276@naver.com>
2018-06-28 05:26:56 +0900

    Update pseudo_code.cpp
    autosave_temp
    REGISTER 번호붙이는거랑 반복/분기문의 LABEL은 나중에-

--- pseudo_code.cpp
+++ pseudo_code.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 
 vector<std::string> pseudo_code;
+int register_num = 1;
+int label_num = 1;
 
 vector<std::string> inorder(ParseNode *current_node) {
 	vector<std::string> variables;
@@ -29,20 +31,80 @@ vector<std::string> inorder(ParseNode *current_node) {
 		variables.clear();
 		break;
 
-	case STAT:
+	case STAT:  // BLOCK, EQUAL, RETURN, while, if-then 
 		if (current_node->child[1]->value.type == EQUAL) {
 			variables = inorder(current_node->child[0]);
-			value = inorder(current_node->child[2]);
-			pseudo_code.push_back("LD " + variables[0] + ", $" + value[0]);
+			value = inorder(current_node->child[2]); 
+			//pseudo_code.push_back("LD " + variables[0] + ", $" + value[0]); 
+			pseudo_code.push_back("LD Reg#" + registor_num + ", " + variables[0]); 	// Register 로 바꿔야되는데		
+		}
+		
+		if (current_node->child[0]->value.type == IF) { 
+			// JUMPF -> make branch
+			value = inorder(current_node->child[1]); 
+			// pseudo_code.push_back("LD " );
+			// pseudo_code.push_back("JUMPT " );
+					
+		}
+			
+		if (current_node->child[0]->value.type == WHILE) { 
+			// LABEL ??
+		}
+			
+		if (current_node->child[0]->value.type == RETURN) { 	
+			// COND를 Reg#1로 return
 		}
 		variables.clear();
 		break;
-
+		
+			
+	case COND: // EXPR < EXPR , EXPR > EXPR , EXPR
+		//value = inorder(current_node->child[0]);
+		value = inorder(current_node->child[0]); 
+		pseudo_code.push_back("LD Reg#" + registor_num + ", " + value);
+		
+		if (current_node->child[1]->value.type == GREATER){
+		value = inorder(current_node->child[1]); 
+		pseudo_code.push_back("LD Reg#" + (registor_num+1) + ", " + value);
+		pseudo_code.push_back("LT Reg#" + registor_num + ", Reg#" + (registor_num+1) + ", Reg#" + registor_num);
+			
+		}else if (current_node->child[1]->value.type == LESS){			
+		value = inorder(current_node->child[1]); 
+		pseudo_code.push_back("LD Reg#" + (registor_num+1) + ", " + value);
+		pseudo_code.push_back("LT Reg#" + registor_num + ", Reg#" + registor_num + ", Reg#" + (registor_num+1));
+		}else{}
+		break;		 
+			
+	case EXPR: // TERM, TERM + TERM
+		value = inorder(current_node->child[0]); 
+		pseudo_code.push_back("LD Reg#" + registor_num + ", " + value);
+		if (current_node->child[1]->value.type == PLUS){			
+			value = inorder(current_node->child[1]); 
+			pseudo_code.push_back("LD Reg#" + (registor_num+1) + ", " + value);
+			pseudo_code.push_back("PLUS Reg#" + registor_num + ", Reg#" + (registor_num+1) + ", Reg#" + registor_num );
+		}else{	}			
+		break;		
+			
+	case TERM: // FACT, FACT * FACT 
+		value = inorder(current_node->child[0]); 
+		pseudo_code.push_back("LD Reg#" + registor_num + ", " + value);
+		if (current_node->child[1]->value.type == MUL){			
+			value = inorder(current_node->child[1]); 
+			pseudo_code.push_back("LD Reg#" + (registor_num+1) + ", " + value);
+			pseudo_code.push_back("MUL Reg#" + registor_num + ", Reg#" + (registor_num+1) + ", Reg#" + registor_num );
+		}else{	}
+		break;	
+			
+	case FACT: // num, word
+		break;
+					      
+								  					      
+					     
 	case INT:
 	case CHAR:
 		variables.push_back(get_type_name((type_t)current_node->value.type));
 		return variables;
-
+			
 	case WORD:
 	case NUM:
 		variables.push_back(current_node->value.value);
