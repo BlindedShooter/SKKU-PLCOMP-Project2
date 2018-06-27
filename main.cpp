393c086ecb1cffd134d2ba8813de407306eacdf6
lojpark <lojpark@gmail.com>
2018-06-27 11:28:54 +0900

    Build foundary parser frame

--- main.cpp
+++ main.cpp
@@ -921,8 +921,36 @@ void scanner(char *file_name)
 void parser()
 {
 	vector<Token> stack;
-	Token temp_token;
+	Token temp_token, stack_token, input_token;
+	bool error_check = false;
 
 	temp_token.type = 0;
 	stack.push_back(temp_token);
+
+	while (true) {
+		stack_token = stack.back();
+		input_token = token_list.back();
+
+		error_check = true;
+		for (int i = 0; i < ptable_n; i++) {
+			if (ptable[i].state == stack_token.type && ptable[i].token == input_token.type) {
+				switch (ptable[i].action) {
+				case SHIFT:
+					break;
+				case REDUCE:
+					break;
+				case GOTO:
+					break;
+				case ACCEPT:
+					break;
+				}
+				error_check = false;
+			}
+		}
+
+		if (error_check) {
+			printf("Compile error.\n");
+			break;
+		}
+	}
 }
